be6f790f8e1f18547c349c0cb1ca01b5229238bb
Tayyab Ali <63280240+tayybi@users.noreply.github.com>
2024-10-03 13:32:48 -0400

    y

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 // The Import Statement in Java
 // You are NOT to include any import statements in your modified code
 
-public class Main {
+public class main {
   
   public static void main(String[] args) {
     java.util.Scanner input = new java.util.Scanner(System.in);
