cb6eb348196d09e15df6f41236084e9b0a2d1cb4
Tayyab Ali <63280240+tayybi@users.noreply.github.com>
2024-10-24 09:33:09 -0400

    c

--- .github/scripts/scanner_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# scanner_check.sh - Hidden logic for Scanner line match check
-
-if grep -Fxq 'java.util.Scanner input = new java.util.Scanner(System.in);' main.java; then
-  exit 0  # Test passed
-else
-  exit 1  # Test failed
-fi
