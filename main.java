c97b01be30df786dec0d8cd5be6eea13a6c714be
Tayyab Ali <63280240+tayybi@users.noreply.github.com>
2024-11-21 18:29:27 -0500

    Update main.java

--- main.java
+++ main.java
@@ -5,8 +5,7 @@
 public class main {
   
   public static void main(String[] args) {
-    // java.util.Scanner input = new java.util.Scanner(System.in);
-
+    
     // System.out.println("What is your name? ");
     // String name = input.next();
     System.out.println("Hi " + "name");
