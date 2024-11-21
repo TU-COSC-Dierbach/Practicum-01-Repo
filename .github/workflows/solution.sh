a5e500108bc9e0c832563cd3bf0d5608a834a235
Tayyab Ali <63280240+tayybi@users.noreply.github.com>
2024-11-21 13:01:34 -0500

    this

--- .github/workflows/solution.sh
+++
@@ -1,8 +0,0 @@
-# #!/bin/bash
-
-# # Check if the required line exists in main.java
-# if grep -q 'java.util.Scanner input = new java.util.Scanner(System.in);' main.java; then
-#     exit 0  # Test passed
-# else
-#     exit 1  # Test failed
-# fi
