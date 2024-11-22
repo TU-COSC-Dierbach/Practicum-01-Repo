479bcfb2b8b6b3c7cb8d0a92843ef278f30dc2ba
Tayyab Ali <63280240+tayybi@users.noreply.github.com>
2024-11-21 22:25:03 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,13 +1,13 @@
 // The Import Statement in Java
 // You are NOT to include any import statements in your modified codejava
 
-
 public class Main {
-  
+
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("What is your name? ");
     String name = input.next();
     System.out.println("Hi " + name); 
   }
+
 }
